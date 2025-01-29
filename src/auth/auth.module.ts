d9037fdbce101ff02c1a000cd3b8e20bbc2b590e
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-01-29 18:24:52 +0530

    feat: added passport  stgrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
