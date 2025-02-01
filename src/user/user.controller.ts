83b4a7b6515d03ec9c15c471008bfeb624faa0fb
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-02-01 13:12:14 +0530

    feat: did some minor refactoring

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
